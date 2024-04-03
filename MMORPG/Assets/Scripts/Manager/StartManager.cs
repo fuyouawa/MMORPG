2e60b7190b5ce49c8c731843c122a13d3840ef47
Noithw <2907142541@qq.com>
2024-04-03 21:57:27 +0800

    修复了几个bug, 完成了Unity客户端的登录注册面板

--- MMORPG/Assets/Scripts/Manager/StartManager.cs
+++ MMORPG/Assets/Scripts/Manager/StartManager.cs
@@ -1,11 +1,76 @@
+using Common.Proto;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 public class StartManager : MonoSingleton<StartManager>
 {
-    public async void ConnectServer()
+    public GameObject AccountPanel;
+    public GameObject LoginPanel;
+    public GameObject RegisterPanel;
+
+    public TMP_InputField LoginUsernameInput;
+    public TMP_InputField LoginPasswordInput;
+    public TMP_InputField RegisterUsernameInput;
+    public TMP_InputField RegisterPasswordInput;
+    public TMP_InputField RegisterVeriftyPasswordInput;
+
+
+    private void Start()
     {
-        await GameClient.Instance.ConnectAsync();
+        AccountPanel.SetActive(true);
+        LoginPanel.SetActive(false);
+        RegisterPanel.SetActive(false);
+        ConnectServer();
+    }
+    public void ConnectServer()
+    {
+        Task.Run(GameClient.Instance.ConnectAsync);
+    }
+
+    public void ShowLogin()
+    {
+        AccountPanel.SetActive(false);
+        LoginPanel.SetActive(true);
+    }
+
+    public void CloseLogin()
+    {
+        AccountPanel.SetActive(true);
+        LoginPanel.SetActive(false);
+    }
+
+    public void ShowRegister()
+    {
+        AccountPanel.SetActive(false);
+        RegisterPanel.SetActive(true);
+    }
+
+    public void CloseRegister()
+    {
+        AccountPanel.SetActive(true);
+        RegisterPanel.SetActive(false);
+    }
+
+    public void TryLogin()
+    {
+        //TODO ’À∫≈√‹¬ÎπÊ∑∂ºÏ≤È
+        GameClient.Instance.Connection.SendRequest(new UserLoginRequest() { 
+            Username = LoginUsernameInput.text,
+            Password = LoginPasswordInput.text,
+        });
+    }
+
+
+    public void TryRegister()
+    {
+        //TODO ’À∫≈√‹¬ÎπÊ∑∂ºÏ≤È
+        GameClient.Instance.Connection.SendRequest(new UserRegisterRequest()
+        {
+            Username = RegisterUsernameInput.text,
+            Password = RegisterPasswordInput.text,
+        });
     }
 }
