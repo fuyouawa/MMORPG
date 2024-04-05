13deb67668702a4d8b05c6e741c7c7f818f99b2c
yuyuaqwq <1454832774@qq.com>
2024-04-05 20:24:00 +0800

    添加GameServer类

--- SERVER/GameServer/Network/TcpServer.cs
+++ SERVER/GameServer/Network/TcpServer.cs
@@ -1,16 +1,15 @@
 ﻿using Common.Network;
+using GameServer.Service;
 using GameServer.Tool;
 using System.Net;
 using System.Net.Sockets;
 
 namespace GameServer.Network
 {
-    public class TcpServer
+    public abstract class TcpServer
     {
         private Socket _serverSocket;
         private List<Channel> _channels = new();
-
-        public event Channel.EventHandler<PacketReceivedEventArgs> PacketReceived;
 
         public TcpServer(int port)
         {
@@ -27,10 +26,11 @@ namespace GameServer.Network
                 var socket = await _serverSocket.AcceptAsync();
                 Global.Logger.Info($"[Server] 客户端连接:{socket.RemoteEndPoint}");
                 Channel channel = new(socket);
-                channel.PacketReceived = PacketReceived;
+                channel.PacketReceived += OnPacketReceived;
                 Task.Run(channel.StartAsync);
                 _channels.Add(channel);
             }
         }
+        protected abstract void OnPacketReceived(object? sender, PacketReceivedEventArgs e);
     }
 }
