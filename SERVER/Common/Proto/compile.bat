9689886ad542892cf1dc6d13091a3f3778776436
yuyuaqwq <1454832774@qq.com>
2024-04-05 19:51:50 +0800

    合并

--- SERVER/Common/Proto/compile.bat
+++ SERVER/Common/Proto/compile.bat
@@ -1,1 +1,2 @@
 protoc --csharp_out=./User ./User/*.proto
+protoc --csharp_out=./Player ./Player/*.proto
---
+++ SERVER/GameServer/Mgr/EntityMgr.cs
@@ -0,0 +1,26 @@
+﻿using GameServer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Mgr
+{
+    public class EntityMgr
+    {
+        private int _serialNum = 1;
+        private Dictionary<int, Entity> _entitiesSet = new();
+
+        public Entity CreateEntity(int spaceId)
+        {
+            lock (this)
+            {
+                var entity = new Entity(_serialNum++, Vector3.Zero, Vector3.Zero);
+                _entitiesSet[entity.EntityId] = entity;
+                return entity;
+            }
+        }
+    }
+}
