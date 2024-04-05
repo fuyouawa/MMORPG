e3123ccc662420ecab5affa3cdc78b5aeb11270c
yuyuaqwq <1454832774@qq.com>
2024-04-05 19:55:54 +0800

    合并

--- SERVER/GameServer/Service/PlayerService.cs
+++ SERVER/GameServer/Service/PlayerService.cs
@@ -1,18 +1,13 @@
 ﻿using Common.Network;
 using Common.Proto.Player;
-using Common.Proto.User;
+using GameServer.Network;
 using GameServer.Tool;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameServer.Service
 {
     public class PlayerService : ServiceBase<PlayerService>
     {
-        public void OnHandle(object? sender, PlayerEnterRequest request)
+        public void OnHandle(Channel? sender, PlayerEnterRequest request)
         {
             Global.Logger.Info($"用户进入游戏");
         }
