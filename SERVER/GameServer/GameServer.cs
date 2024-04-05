1cbd35a97a328571242dfc9a6cf6bd44ab626148
yuyuaqwq <1454832774@qq.com>
2024-04-05 20:24:00 +0800

    添加GameServer类

---
+++ SERVER/GameServer/GameServer.cs
@@ -0,0 +1,22 @@
+﻿using Common.Network;
+using GameServer.Network;
+using GameServer.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    public class GameServer : TcpServer
+    {
+        public GameServer(int port) : base(port) { }
+
+        protected override void OnPacketReceived(object? sender, PacketReceivedEventArgs e)
+        {
+            UserService.Instance.HandleMessage(sender, e.Packet.Message);
+            PlayerService.Instance.HandleMessage(sender, e.Packet.Message);
+        }
+    }
+}
