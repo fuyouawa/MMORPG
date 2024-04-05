473a20fa5ca748a7513b127929b1dc7005b51a5f
Noithw <2907142541@qq.com>
2024-04-05 18:05:06 +0800

    扩展功能, 增加客户端可以ReceiveAsync, 优化代码, 修复bug

--- SERVER/GameServer/Service/NetService.cs
+++ SERVER/GameServer/Service/NetService.cs
@@ -12,9 +12,10 @@ namespace GameServer.Service
 {
     public class NetService : ServiceBase<NetService>
     {
-        public void OnHandle(object? sender, UserLoginRequest request)
+        public void OnHandle(Connection sender, UserLoginRequest request)
         {
             Global.Logger.Info($"[Service] 用户注册请求: Username={request.Username}, Password={request.Password}");
+            sender.SendAsync(new UserLoginResponse(), null);
         }
     }
 }
