995fff4e05ff079013ea8fd31923871187f3f148
yuyuaqwq <1454832774@qq.com>
2024-04-05 19:55:54 +0800

    合并

--- SERVER/GameServer/Service/UserService.cs
+++ SERVER/GameServer/Service/UserService.cs
@@ -1,17 +1,13 @@
 ﻿using Common.Network;
 using Common.Proto.User;
+using GameServer.Network;
 using GameServer.Tool;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameServer.Service
 {
     public class UserService : ServiceBase<UserService>
     {
-        public void OnHandle(Connection? sender, UserLoginRequest request)
+        public void OnHandle(Channel? sender, UserLoginRequest request)
         {
             UserLoginResponse a;
             Global.Logger.Info($"用户登录请求: Username={request.Username}, Password={request.Password}");
