92a4fe628c2a07d3f91acacfcbd5b7644c15b1e5
arlozen <arlozen.dev@gmail.com>
2024-04-07 16:49:39 +0800

    新增SpinnerBox, PanelSwitcher

--- MMORPG/Assets/Scripts/Manager/StartManager.cs
+++ MMORPG/Assets/Scripts/Manager/StartManager.cs
@@ -8,13 +8,65 @@ using UnityEngine;
 
 public class StartManager : MonoSingleton<StartManager>
 {
+    public GameObject AccountPanel;
+    public GameObject LoginPanel;
+    public GameObject RegisterPanel;
+
     void Start()
     {
+        AccountPanel.SetActive(true);
+        LoginPanel.SetActive(false);
+        RegisterPanel.SetActive(false);
         ConnectServer();
     }
 
     public void ConnectServer()
     {
-        Task.Run(GameClient.Instance.ConnectAsync);
+        Task.Run(async () =>
+        {
+            while (true)
+            {
+                SpinnerBox.Show("连接服务器中......");
+                try
+                {
+                    await GameClient.Instance.ConnectAsync();
+                    SpinnerBox.Close();
+                    break;
+                }
+                catch (System.Exception ex)
+                {
+                    SpinnerBox.Close();
+                    await MessageBox.ShowErrorAsync($"连接服务器失败:{ex}", buttonText:"重新连接");
+                    continue;
+                }
+            }
+            //TODO 异常处理
+            await GameClient.Instance.StartAsync();
+        });
     }
+
+    public void ShowLogin()
+    {
+        PanelSwitcher.FadeOut(AccountPanel);
+        PanelSwitcher.FadeIn(LoginPanel);
+    }
+
+    public void CloseLogin()
+    {
+        PanelSwitcher.FadeIn(AccountPanel);
+        PanelSwitcher.FadeOut(LoginPanel);
+    }
+
+    public void ShowRegister()
+    {
+        PanelSwitcher.FadeOut(AccountPanel);
+        PanelSwitcher.FadeIn(RegisterPanel);
+    }
+
+    public void CloseRegister()
+    {
+        PanelSwitcher.FadeIn(AccountPanel);
+        PanelSwitcher.FadeOut(RegisterPanel);
+    }
+
 }
