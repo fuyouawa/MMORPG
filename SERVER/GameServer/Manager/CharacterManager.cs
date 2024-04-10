ab2f86674cb9530c2d963b1c8ded3a00ef6158db
yuyuaqwq <1454832774@qq.com>
2024-04-10 15:56:40 +0800

    传输坐标使用int型

---
+++ SERVER/GameServer/Manager/CharacterManager.cs
@@ -0,0 +1,14 @@
+﻿using Common.Tool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Manager
+{
+    public class CharacterManager : Singleton<EntityManager>
+    {
+
+    }
+}
