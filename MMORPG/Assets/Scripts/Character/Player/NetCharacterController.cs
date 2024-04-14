e61d727282b51b86774d3d5082dcc8781857de70
yuyuaqwq <1454832774@qq.com>
2024-04-14 17:38:29 +0800

    完善角色控制器及动画播放

--- MMORPG/Assets/Scripts/Character/Player/NetCharacterController.cs
+++ MMORPG/Assets/Scripts/Character/Player/NetCharacterController.cs
@@ -7,13 +7,13 @@ public class NetCharacterController : MonoBehaviour
     public float MoveSpeed;
     public float RotateLerp;
 
-    private Rigidbody _rigidbody;
-
     protected bool _isMine = true;
+
+    private NetCharacterAnimator _animator;
 
     private void Awake()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<NetCharacterAnimator>();
     }
 
     void FixedUpdate()
@@ -21,34 +21,69 @@ public class NetCharacterController : MonoBehaviour
         if (_isMine)
         {
             ControlPlayer();
+
+
         }
     }
-
 
     protected virtual void ControlPlayer()
     {
-        // 获取摄像机的前方向，即摄像机的观察方向
-        var cameraForward = Camera.main.transform.forward;
-        cameraForward.y = 0f; // 忽略y轴，保持在水平方向
-
-        // 获取输入方向
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-
-        // 如果有输入，计算移动方向，基于摄像机的前方向
-        Vector3 moveDirection = Vector3.zero;
-        if (inputDirection != Vector3.zero)
+        if (Input.GetMouseButton(0))
         {
-            moveDirection = Quaternion.LookRotation(cameraForward) * inputDirection;
+            PlayerAttack();
         }
-        // 移动角色
-        _rigidbody.velocity = moveDirection * MoveSpeed;
 
-        // 如果有输入，旋转角色朝向移动方向
-        if (inputDirection != Vector3.zero)
-        {
-            transform.forward = Vector3.Lerp(transform.forward, moveDirection.normalized, RotateLerp);
+        else if(_animator.Status != NetCharacterAnimator.AnimationStatus.Attack) {
+            // 获取摄像机的前方向，即摄像机的观察方向
+            var cameraForward = Camera.main.transform.forward;
+            cameraForward.y = 0f; // 忽略y轴，保持在水平方向
+
+            CharacterController controller = GetComponent<CharacterController>();
+
+            // 获取输入方向
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
+            Vector3 inputDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+            Vector3 moveDirection = Vector3.zero;
+            if (inputDirection != Vector3.zero)
+            {
+                Debug.Log(horizontalInput);
+                Debug.Log(verticalInput);
+
+                moveDirection = Quaternion.LookRotation(cameraForward) * inputDirection;
+
+                PlayerRun();
+            }
+            else
+            {
+                PlayerIdle();
+            }
+
+            controller.SimpleMove(moveDirection * MoveSpeed);
+
+            // 如果有输入，旋转角色朝向移动方向
+            if (inputDirection != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(moveDirection.normalized, Vector3.up);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, RotateLerp);
+            }
         }
     }
+
+    protected virtual void PlayerAttack()
+    {
+        _animator.PlayAttack01();
+    }
+
+    protected virtual void PlayerRun()
+    {
+        _animator.PlayRun();
+    }
+
+    protected virtual void PlayerIdle()
+    {
+        _animator.PlayIdle();
+    }
+
 }
