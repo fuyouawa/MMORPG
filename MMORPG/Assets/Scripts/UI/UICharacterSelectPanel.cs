c1ff52a6c1123bef0be5f766fdefa70a3b7bb627
yuyuaqwq <1454832774@qq.com>
2024-04-26 15:22:45 +0800

    开始游戏场景

--- MMORPG/Assets/Scripts/UI/UICharacterSelectPanel.cs
+++ MMORPG/Assets/Scripts/UI/UICharacterSelectPanel.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
 using QFramework;
+using UnityEngine.SceneManagement;
 
 namespace MMORPG.UI
 {
 	public class UICharacterSelectPanelData : UIPanelData
 	{
 	}
-	public partial class UICharacterSelectPanel : UIPanel
-	{
-		protected override void OnInit(IUIData uiData = null)
+	public partial class UICharacterSelectPanel : UIPanel, IController
+    {
+        public IArchitecture GetArchitecture()
+        {
+            return GameApp.Interface;
+        }
+
+        protected override void OnInit(IUIData uiData = null)
 		{
 			mData = uiData as UICharacterSelectPanelData ?? new UICharacterSelectPanelData();
-			// please add init code here
-		}
+            // please add init code here
+
+            BtnPlay.onClick.AddListener(() => JoinMapScene(1));
+        }
 		
 		protected override void OnOpen(IUIData uiData = null)
 		{
@@ -30,5 +38,17 @@ namespace MMORPG.UI
 		protected override void OnClose()
 		{
 		}
-	}
+
+
+        private void JoinMapScene(int mapId)
+        {
+            //TODO mapid
+            var op = SceneManager.LoadSceneAsync("Space1Scene");
+            op.completed += _ =>
+            {
+                this.GetSystem<IMapManagerSystem>().JoinedMap(mapId);
+            };
+        }
+
+    }
 }
