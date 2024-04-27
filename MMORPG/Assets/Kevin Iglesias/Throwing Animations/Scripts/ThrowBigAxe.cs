4c0cc9a446a2b51d6236763a13be35e39170a8fc
arlozen <arlozen.dev@gmail.com>
2024-04-27 22:29:52 +0800

    优化代码

---
+++ MMORPG/Assets/Kevin Iglesias/Throwing Animations/Scripts/ThrowBigAxe.cs
@@ -0,0 +1,138 @@
+﻿///////////////////////////////////////////////////////////////////////////
+//  ThrowBigAxe                                                          //
+//  Kevin Iglesias - https://www.keviniglesias.com/       			     //
+//  Contact Support: support@keviniglesias.com                           //
+///////////////////////////////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KevinIglesias {
+    
+    public class ThrowBigAxe : MonoBehaviour {
+
+        //Retargeter
+        public Transform retargeter;
+
+        //Prop to move
+        public Transform propToSpin;
+        
+        //Hand that holds the prop
+        public Transform hand;
+        
+        //How far will the prop launched
+        public float spinDistance;
+        
+        //Movement speed of the prop
+        public float translationSpeed;
+        
+        //Rotation speed of the prop
+        public float spinSpeed;
+        
+        //Needed for check if the trick is active
+        public bool spinActive = false;
+        
+        //Offset for fitting the prop in end distance
+        public Vector3 endPositionOffset;
+        
+        //Offset for fitting the prop in hand when returning
+        public Vector3 returningPositionOffset;
+        
+        //Character root (for parenting when prop is thrown)
+        private Transform characterRoot;
+        //Needed for getting the prop back
+        private Vector3 zeroPosition;
+        private Quaternion zeroRotation;
+        //Needed for calculate prop trajectory
+        private Vector3 startPosition;
+        private Quaternion startRotation;
+        private Vector3 endPosition;
+        private Quaternion endRotation;
+        //Coroutine that will make the prop move
+        private IEnumerator spinCO;
+
+        public void Awake()
+        {
+            characterRoot = this.transform;
+            
+            zeroPosition = propToSpin.localPosition;
+            zeroRotation = propToSpin.localRotation;
+        }
+        
+        public void Update()
+        {
+            if(retargeter.localPosition.y > 0)
+            {
+                if(!spinActive)
+                {
+                    SpinProp();
+                    spinActive = true;
+                }
+            }else{
+                
+                if(spinActive)
+                {
+                    if(spinCO != null)
+                    {
+                        StopCoroutine(spinCO);
+                    }
+                    propToSpin.SetParent(hand);
+                    propToSpin.localPosition = zeroPosition;
+                    propToSpin.localRotation = zeroRotation;
+                }
+                spinActive = false;
+            }
+        }
+    
+        //Function called when retargeter is active
+        public void SpinProp()
+        {
+            if(spinCO != null)
+            {
+                StopCoroutine(spinCO);
+            }
+            spinCO = StartSpin();
+            StartCoroutine(spinCO);
+        }
+        
+        IEnumerator StartSpin()
+        {
+            //Remove prop from hand
+            propToSpin.SetParent(characterRoot);
+            
+            //Get initial position/rotation
+            startPosition = propToSpin.position;
+            startRotation = propToSpin.localRotation;
+
+            //Set end position (farthest point the prop will get)
+            endPosition = new Vector3(propToSpin.position.x-spinDistance, propToSpin.position.y, propToSpin.position.z);
+            endPosition = endPosition+endPositionOffset;
+            
+            //Going away
+            float i = 0;
+            while(i < 1f)
+            {
+                
+                i += Time.deltaTime * translationSpeed;
+
+                propToSpin.position = Vector3.Lerp(startPosition, endPosition, Mathf.Sin(i * Mathf.PI * 0.5f));
+                propToSpin.transform.Rotate(0.0f, -spinSpeed, 0.0f, Space.World);
+                yield return 0;
+            }
+            
+            //Coming back
+            i = 0;
+            while(i < 1f)
+            {
+                i += Time.deltaTime * translationSpeed;
+                
+                propToSpin.position = Vector3.Lerp(endPosition, startPosition+returningPositionOffset, 1f - Mathf.Cos(i * Mathf.PI * 0.5f));
+                propToSpin.transform.Rotate(0f, -spinSpeed, 0.0f, Space.World);
+                
+                yield return 0;
+            }
+        }
+        
+    }
+}
--- MMORPG/Assets/Scripts/Game/Player/HeroKnight.cs
+++ MMORPG/Assets/Scripts/Game/Player/HeroKnight.cs
@@ -6,55 +6,27 @@ using UnityEngine;
 
 public class HeroKnight : MonoBehaviour, INetworkEntityCallbacks
 {
-    [AnimatorParam]
-    [ReadOnly]
-    public bool Walking;
-    [AnimatorParam]
-    [ReadOnly]
-    public float HoriSpeedNormalized;
-    [AnimatorParam]
-    [ReadOnly]
-    public float VertSpeedNormalized;
-
-    [Range(0.1f, 100f)]
-    public float Acceleration = 3;
-
-    private AnimatorMachine _animatorMachine;
-    [SerializeField]
-    [ReadOnly]
-    private Vector2 _moveAxis;
+    private PlayerAnimatorController _animatorController;
     private GameInputControls _inputControls;
+    private Vector2 _moveInput;
     private Entity _entity;
 
     void Awake()
     {
         _inputControls = new();
-        _animatorMachine = new(this, GetComponent<Animator>());
         _entity = GetComponent<Entity>();
+        _animatorController = GetComponentInChildren<PlayerAnimatorController>();
     }
 
-    private void Update()
+    private void Start()
     {
-        _animatorMachine.UpdateAnimator();
+        _animatorController.Setup(transform, _entity.IsMine);
     }
 
     void INetworkEntityCallbacks.NetworkMineUpdate()
     {
-        _moveAxis = _inputControls.Player.Move.ReadValue<Vector2>();
-        var speedNormalized = Vector2.MoveTowards(
-            new Vector2(HoriSpeedNormalized, VertSpeedNormalized),
-            _moveAxis,
-            Acceleration * Time.deltaTime);
-
-        Walking = _moveAxis.sqrMagnitude > 0.5f || speedNormalized.sqrMagnitude > 0.5f;
-        HoriSpeedNormalized = speedNormalized.x;
-        VertSpeedNormalized = speedNormalized.y;
-
-        if (!(_moveAxis.sqrMagnitude > 0.5f)) return;
-        var cameraForward = Camera.main.transform.forward;
-        cameraForward.y = 0;
-        var targetRotation = Quaternion.LookRotation(cameraForward, Vector3.up);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 0.2f);
+        _moveInput = _inputControls.Player.Move.ReadValue<Vector2>();
+        _animatorController.SetMovement(_moveInput);
     }
 
     private void OnEnable()
@@ -66,13 +38,4 @@ public class HeroKnight : MonoBehaviour, INetworkEntityCallbacks
     {
         _inputControls.Disable();
     }
-
-    //private void OnAnimatorMove()
-    //{
-    //    if (_entity.IsMine)
-    //    {
-    //        transform.position = _animatorMachine.Animator.deltaPosition;
-    //        transform.rotation = _animatorMachine.Animator.deltaRotation;
-    //    }
-    //}
 }
