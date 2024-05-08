1c5a1ff81fd42886c32484f9d0ae86c8de358705
Noithw <2907142541@qq.com>
2024-05-08 22:05:34 +0800

    完善代码

--- MMORPG/Assets/Scripts/Game/Launch/ExitMapState.cs
+++ MMORPG/Assets/Scripts/Game/Launch/ExitMapState.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 public class ExitMapState : AbstractState<LaunchStatus, LaunchController>, IController
 {
@@ -16,6 +17,11 @@ public class ExitMapState : AbstractState<LaunchStatus, LaunchController>, ICont
     protected override void OnEnter()
     {
         Logger.Info("Launch", "退出地图");
+        var mapMgr = this.GetSystem<IMapManagerSystem>();
+        Debug.Assert(mapMgr.IsInMap);
+        mapMgr.ExitMap();
+
+        mFSM.ChangeState(LaunchStatus.WaitForJoinMap);
     }
 
     public IArchitecture GetArchitecture()
