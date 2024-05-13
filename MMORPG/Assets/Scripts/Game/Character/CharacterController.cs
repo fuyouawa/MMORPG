32c56527fd0b28f1a95069ecb09ab88082fc2524
Noithw <2907142541@qq.com>
2024-05-13 21:28:01 +0800

    修复了几个bug

--- MMORPG/Assets/Scripts/Game/Character/CharacterController.cs
+++ MMORPG/Assets/Scripts/Game/Character/CharacterController.cs
@@ -35,65 +35,35 @@ namespace MMORPG.Game
 
         private INetworkSystem _newtwork;
 
-        private Vector3 _targetPosition;
-        private Quaternion _targetRotation;
-
-        private bool _needMove = false;
-        private bool _needRotate = false;
-
         private void Awake()
         {
             _newtwork = this.GetSystem<INetworkSystem>();
             Rigidbody = GetComponent<Rigidbody>();
             Collider = GetComponent<CapsuleCollider>();
-            _targetRotation = transform.rotation;
-            _targetPosition = transform.position;
         }
 
         private void Update()
         {
-            if (_needMove)
-            {
-                if (Vector3.Distance(transform.position, _targetPosition).Abs() > MoveSmooth * Time.deltaTime)
-                {
-                    transform.position = Vector3.MoveTowards(transform.position, _targetPosition, MoveSmooth * Time.deltaTime);
-                }
-                else
-                {
-                    transform.position = _targetPosition;
-                    _needMove = false;
-                }
-            }
-
-            if (_needRotate)
-            {
-                if (Quaternion.Dot(transform.rotation, _targetRotation).Abs() < 0.95f)
-                {
-                    transform.rotation = Quaternion.Lerp(transform.rotation, _targetRotation, RotationSmooth * Time.deltaTime);
-                }
-                else
-                {
-                    transform.rotation = _targetRotation;
-                    _needRotate = false;
-                }
-            }
         }
 
         public void SmoothRotate(Quaternion targetRotation)
         {
-            _needRotate = true;
-            _targetRotation = targetRotation;
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, RotationSmooth * Time.deltaTime);
         }
 
         public void SmoothMove(Vector3 position)
         {
-            _needMove = true;
-            _targetPosition = position;
+            transform.position = Vector3.Lerp(transform.position, position, MoveSmooth * Time.deltaTime);
         }
 
-        public void SmoothMoveDirection(Vector3 direction)
+        public void MoveDirection(Vector3 direction)
         {
-            _targetPosition += direction;
+            transform.position += direction;
+        }
+
+        public void RelativeRotate(Quaternion rotation)
+        {
+            transform.rotation *= rotation;
         }
 
         public void NetworkUploadTransform(int stateId, byte[] data)
--- MMORPG/Assets/Scripts/Game/Character/Player/LocalHeroKnightMovement.cs
+++ MMORPG/Assets/Scripts/Game/Character/Player/LocalHeroKnightMovement.cs
@@ -17,14 +17,10 @@ namespace MMORPG.Game
         public float BackIdleThreshold = 0.5f;
 
         private Vector2 _moveDirection;
-        private Vector2 _inputDirection;
         private bool _prevMoveForward;
         private bool? _forwardSidle;
 
-        private void Update()
-        {
-            _inputDirection = Brain.InputControls.Player.Move.ReadValue<Vector2>();
-        }
+        private Vector2 InputDirection => Brain.InputControls.Player.Move.ReadValue<Vector2>();
 
         public override void OnStateInit()
         {
@@ -56,7 +52,7 @@ namespace MMORPG.Game
         [StateCondition]
         public bool ReachIdleThreshold()
         {
-            return _inputDirection.magnitude > IdleThreshold;
+            return InputDirection.magnitude > IdleThreshold;
         }
 
         [StateCondition]
@@ -67,18 +63,18 @@ namespace MMORPG.Game
 
         private void ControlMove()
         {
-            _inputDirection = Brain.InputControls.Player.Move.ReadValue<Vector2>();
-            TransformMoveDirection();
+            var dir = InputDirection;
+            TransformMoveDirection(dir);
             if (_prevMoveForward)
             {
-                if (_inputDirection.y < -0.5f)
+                if (dir.y < -0.5f)
                 {
                     _prevMoveForward = false;
                 }
             }
             else
             {
-                if (_inputDirection.y > 0.5f)
+                if (dir.y > 0.5f)
                 {
                     _prevMoveForward = true;
                 }
@@ -99,12 +95,12 @@ namespace MMORPG.Game
             Brain.CharacterController.SmoothRotate(targetRotation);
         }
 
-        private void TransformMoveDirection()
+        private void TransformMoveDirection(Vector2 dir)
         {
-            _moveDirection = _inputDirection;
+            _moveDirection = dir;
 
-            var xMove = !Mathf.Approximately(_inputDirection.x, 0);
-            var yMove = !Mathf.Approximately(_inputDirection.y, 0);
+            var xMove = !Mathf.Approximately(dir.x, 0);
+            var yMove = !Mathf.Approximately(dir.y, 0);
 
             if ((!xMove && yMove) || (xMove && yMove))
             {
