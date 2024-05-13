a43c31ff3f38410a08101aebd3f68cd17cb4179f
arlozen <arlozen.dev@gmail.com>
2024-05-13 19:33:43 +0800

    优化动作切换

--- MMORPG/Assets/Scripts/Game/Character/Player/Core/PlayerAnimationController.cs
+++ MMORPG/Assets/Scripts/Game/Character/Player/Core/PlayerAnimationController.cs
@@ -1,4 +1,6 @@
 using MMORPG.Tool;
+using QFramework;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace MMORPG.Game
@@ -7,7 +9,9 @@ namespace MMORPG.Game
     {
         public GameObject Owner;
 
-        public float MoveAcceleration = 3f;
+        [Title("Move Switch")]
+        public float MoveSwitchVelocity = 3f;
+        public float MoveSwitchAcceleration = 3f;
 
         private Animator _animator;
         private bool _animatorMove = true;
@@ -20,11 +24,45 @@ namespace MMORPG.Game
         [AnimatorParam]
         public float VertMovementDirection { get; set; }
 
-        public Vector2 MovementDirection => new(HoriMovementDirection, VertMovementDirection);
+        public Vector2 MovementDirection
+        {
+            get => new(HoriMovementDirection, VertMovementDirection);
+            set
+            {
+                HoriMovementDirection = value.x;
+                VertMovementDirection = value.y;
+            }
+        }
 
         private Vector2 _targetMoveDirection;
 
         private AnimatorMachine _machine;
+
+        private float _currentMoveSwitchVelocity;
+
+        private static readonly float s_moveExtraRectCoefficient = 0.7f;
+        private static readonly float s_moveExtraMaxAcceleration = 6;
+
+        private static readonly Rect s_moveExtraAccelerationRect = new(
+            -s_moveExtraRectCoefficient,
+            -s_moveExtraRectCoefficient,
+            s_moveExtraRectCoefficient * 2,
+            s_moveExtraRectCoefficient * 2);
+
+        private static float GetExtraAccelerationCoefficient(Vector2 pos)
+        {
+            if (pos.x.Abs() < 0.01f && pos.y.Abs() < 0.01f)
+                return s_moveExtraMaxAcceleration;
+            if (!s_moveExtraAccelerationRect.Contains(pos))
+                return 0;
+            var stdPos = pos * (1 / s_moveExtraRectCoefficient);
+
+            var E = new Vector2((stdPos.x / stdPos.y).Abs(), 1);
+            var radio = 1 / E.magnitude;
+            var len = stdPos.magnitude * radio;
+
+            return (1 - len) * s_moveExtraMaxAcceleration;
+        }
 
         public void SmoothMoveDirection(Vector2 dir)
         {
@@ -32,17 +70,17 @@ namespace MMORPG.Game
         }
 
 
-
         void Awake()
         {
             _animator = GetComponent<Animator>();
             _machine = new(this, gameObject, _animator);
             _machine.Run();
+            _currentMoveSwitchVelocity = MoveSwitchVelocity;
         }
 
         void Update()
         {
-            SmoothMoving();
+            Moving();
         }
 
         public void EnableAnimatorMove()
@@ -55,11 +93,26 @@ namespace MMORPG.Game
             _animatorMove = false;
         }
 
-        private void SmoothMoving()
+        private void Moving()
         {
-            var acc = MoveAcceleration * Time.deltaTime;
-            HoriMovementDirection = Mathf.MoveTowards(HoriMovementDirection, _targetMoveDirection.x, acc);
-            VertMovementDirection = Mathf.MoveTowards(VertMovementDirection, _targetMoveDirection.y, acc);
+            if (Vector2.Distance(MovementDirection, _targetMoveDirection) > 0.1f)
+            {
+                var targetMoveDir = _targetMoveDirection;
+                if (_targetMoveDirection.y * VertMovementDirection < -0.1f ||
+                     _targetMoveDirection.x * HoriMovementDirection < -0.1f)
+                {
+                    targetMoveDir = Vector2.zero;
+                }
+
+                _currentMoveSwitchVelocity += Time.deltaTime * MoveSwitchAcceleration;
+                var velocity = MoveSwitchVelocity + GetExtraAccelerationCoefficient(MovementDirection);
+                MovementDirection = Vector2.MoveTowards(MovementDirection, targetMoveDir, velocity * Time.deltaTime);
+            }
+            else
+            {
+                MovementDirection = _targetMoveDirection;
+                _currentMoveSwitchVelocity = MoveSwitchVelocity;
+            }
         }
 
         private void OnAnimatorMove()
