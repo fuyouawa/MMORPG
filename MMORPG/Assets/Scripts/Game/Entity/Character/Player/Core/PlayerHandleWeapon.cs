2d341bf7b7635900714e3e3083ddff37021b8d6b
Noithw <2907142541@qq.com>
2024-05-16 12:06:50 +0800

    完善持有武器

--- MMORPG/Assets/Scripts/Game/Entity/Character/Player/Core/PlayerHandleWeapon.cs
+++ MMORPG/Assets/Scripts/Game/Entity/Character/Player/Core/PlayerHandleWeapon.cs
@@ -1,18 +1,102 @@
 using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
  namespace MMORPG.Game
 {
+    public enum HandleWeaponMode
+    {
+        LeftHand,
+        RightHand
+    }
+
     public class PlayerHandleWeapon : MonoBehaviour
     {
-        public PlayerBrain Brain { get; private set; }
+        [Title("Weapon")]
+        public Weapon InitialWeapon;
+
+        [ReadOnly]
+        [ShowInInspector]
+        public Weapon CurrentWeapon { get; private set; }
+        [Title("Binding")]
+        public Transform WeaponAttachment;
+        public HandleWeaponMode HandleMode = HandleWeaponMode.RightHand;
+        public Transform LeftHandTarget;
+        public Transform RightHandTarget;
+
+        [Button]
+        private void UpdateWeaponAttachmentTransform()
+        {
+            if (WeaponAttachment)
+            {
+                switch (HandleMode)
+                {
+                    case HandleWeaponMode.LeftHand:
+                        if (LeftHandTarget)
+                            WeaponAttachment.SetPositionAndRotation(LeftHandTarget.position, LeftHandTarget.rotation);
+                        break;
+                    default:
+                        if (RightHandTarget)
+                            WeaponAttachment.SetPositionAndRotation(RightHandTarget.position, RightHandTarget.rotation);
+                        break;
+                }
+            }
+        }
 
         public delegate void WeaponChangedHandler(Weapon current, Weapon previous);
         public event WeaponChangedHandler OnWeaponChanged;
 
-        public void ChangeWeapon(Weapon newWeapon)
+        public PlayerBrain Brain { get; private set; }
+
+        private void Awake()
         {
-            
+            if (InitialWeapon)
+            {
+                ChangeWeapon(InitialWeapon);
+            }
+        }
+
+        private void Update()
+        {
+            UpdateWeaponAttachmentTransform();
+        }
+
+        public void ChangeWeapon(Weapon newWeapon, bool combo = false)
+        {
+            if (CurrentWeapon)
+            {
+                CurrentWeapon.TurnWeaponOff();
+                if (!combo)
+                {
+                    //TODO 销毁武器
+                }
+            }
+
+            var tmp = CurrentWeapon;
+            if (newWeapon != null)
+            {
+                InstantiateWeapon(newWeapon, combo);
+            }
+            else
+            {
+                CurrentWeapon = null;
+            }
+            OnWeaponChanged?.Invoke(newWeapon, tmp);
+        }
+
+        private void InstantiateWeapon(Weapon newWeapon, bool combo)
+        {
+            if (!combo)
+            {
+                CurrentWeapon = Instantiate(newWeapon,
+                    WeaponAttachment.transform.position + newWeapon.WeaponAttachmentOffset,
+                    WeaponAttachment.transform.rotation);
+            }
+
+            CurrentWeapon.transform.parent = WeaponAttachment.transform;
+            CurrentWeapon.transform.localPosition = newWeapon.WeaponAttachmentOffset;
+            CurrentWeapon.Setup(Brain);
+            CurrentWeapon.Initialize();
         }
 
         public void Setup(PlayerBrain brain)
