b21b33e5dbee9ac84060200e6624ca7c7c5fd1e7
yuyuaqwq <1454832774@qq.com>
2024-05-24 00:42:49 +0800

    怪物追击ai测试

--- SERVER/Common/Proto/Character/NetCharacter.cs
+++ SERVER/Common/Proto/Character/NetCharacter.cs
@@ -39,6 +39,7 @@ namespace Common.Proto.Character {
 
   }
   #region Messages
+  [global::System.Diagnostics.DebuggerDisplayAttribute("{ToString(),nq}")]
   public sealed partial class NetCharacter : pb::IMessage<NetCharacter>
   #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
       , pb::IBufferMessage
