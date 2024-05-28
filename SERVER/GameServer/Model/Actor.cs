7e79d6a08379b73c8ac832630c812de9095ff3f7
yuyuaqwq <1454832774@qq.com>
2024-05-29 01:51:07 +0800

    怪物移动、攻击动画添加

--- SERVER/GameServer/Model/Actor.cs
+++ SERVER/GameServer/Model/Actor.cs
@@ -4,33 +4,49 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using GameServer.Fight;
+using GameServer.Manager;
 
 namespace GameServer.Model
 {
     public enum ActorState
     {
-        Idle = 1,
-        Move = 2,
+        None = 0,
+        Idle,
+        Move,
+        Attack,
     }
 
     public class Actor : Entity
     {
         public ActorState State;
         public string Name;
-        public int Speed;
         public int Level;
-        public int Hp;
-        public int Mp;
+        public AttributeManager AttributeManager;
+        public SkillManager SkillManager;
 
         public Actor(Map map, string name)
         {
             Map = map;
             Name = name;
+            SkillManager = new(this);
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            SkillManager.Update();
         }
 
         public bool IsDeath()
         {
-            return Hp <= 0;
+            return false;
+            //return Hp <= 0;
         }
     }
 }
