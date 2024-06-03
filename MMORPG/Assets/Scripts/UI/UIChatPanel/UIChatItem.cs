3e5b74276e0e0c7d7bd1b9e9d4b93071c512e78a
Noithw <2907142541@qq.com>
2024-06-03 22:27:20 +0800

    完成聊天系统UI

---
+++ MMORPG/Assets/Scripts/UI/UIChatPanel/UIChatItem.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using QFramework;
+using System;
+using MMORPG.Model;
+using MMORPG.Tool;
+
+namespace MMORPG.UI
+{
+    public enum ChatMessageType
+    {
+        World,
+        Map,
+        Group
+    }
+
+    public partial class UIChatItem : ViewController
+    {
+		void Start()
+		{
+			// Code Here
+		}
+
+        public void Setup(
+            DateTime sendTime,
+            ChatMessageType type,
+            string message,
+            Color messageColor,
+            bool isComposite)
+        {
+            var characterName = this.GetModel<IUserModel>().CharacterName.Value;
+
+            var messageColorHex = messageColor.ToHex();
+
+            if (isComposite)
+            {
+                var typeStr = type switch
+                {
+                    ChatMessageType.World => "世界",
+                    ChatMessageType.Group => "组队",
+                    ChatMessageType.Map => "地图",
+                    _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+                };
+
+                TextMessage.SetText("<b>" +
+                                    $"<color={messageColorHex}>[{sendTime:HH:mm:ss}][{typeStr}]</color>" +
+                                    $"<color=#b59e8aff>[{characterName}]</color>" +
+                                    "</b>" +
+                                    $":{message}");
+            }
+            else
+            {
+                TextMessage.SetText("<b>" +
+                                    $"<color={messageColorHex}>[{sendTime:HH:mm:ss}]</color>" +
+                                    $"<color=#b59e8aff>[{characterName}]</color>" +
+                                    "</b>" +
+                                    $":{message}");
+            }
+        }
+    }
+}
