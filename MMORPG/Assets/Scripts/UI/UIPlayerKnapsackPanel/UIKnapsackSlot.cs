b594ccf16e5735465c8f18565b810cdc6d3066c4
计都 <119787492+fuyouawa@users.noreply.github.com>
2024-06-09 14:59:33 +0800

    优化槽代码

--- MMORPG/Assets/Scripts/UI/UIPlayerKnapsackPanel/UIKnapsackSlot.cs
+++ MMORPG/Assets/Scripts/UI/UIPlayerKnapsackPanel/UIKnapsackSlot.cs
@@ -1,16 +1,61 @@
+using Common.Inventory;
+using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace MMORPG.UI
 {
     public class UIKnapsackSlot : UISlotBase
     {
-        public Image Image { get; protected set; }
+        public TextMeshProUGUI TextAmount;
+        public Image ImageIcon;
 
-        protected override void Awake()
+        /// <summary>
+        /// Item信息
+        /// </summary>
+        public Item Item { get; protected set; }
+
+        private int _amount;
+
+        /// <summary>
+        /// 物品数量
+        /// </summary>
+        public int Amount
         {
-            base.Awake();
-            Image = transform.Find("Icon").GetComponent<Image>();
+            get => _amount;
+            set
+            {
+                _amount = value;
+                if (TextAmount != null)
+                {
+                    TextAmount.SetText(value == 0 ? string.Empty : value.ToString());
+                }
+            }
         }
-       
+
+        /// <summary>
+        /// 将Item赋值给此槽
+        /// </summary>
+        /// <param name="item"></param>
+        public void Assign(Item item)
+        {
+            Debug.Assert(item.SlotId == SlotId);
+            Amount = item.Amount;
+            ImageIcon.enabled = true;
+            ImageIcon.sprite = Resources.Load<Sprite>(item.Icon);
+            //TODO 其他可能的处理
+            Item = item;
+        }
+
+        /// <summary>
+        /// 将此槽置空
+        /// </summary>
+        public void SetEmpty()
+        {
+            ImageIcon.enabled = false;
+            ImageIcon.sprite = null;
+            Amount = 0;
+            Item = null;
+        }
     }
 }
