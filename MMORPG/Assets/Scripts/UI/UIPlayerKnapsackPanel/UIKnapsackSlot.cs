15710dac6f5fa6ff87436ca15eb8d3da335e291f
yuyuaqwq <1454832774@qq.com>
2024-06-09 15:04:49 +0800

    合并

--- MMORPG/Assets/Scripts/UI/UIPlayerKnapsackPanel/UIKnapsackSlot.cs
+++ MMORPG/Assets/Scripts/UI/UIPlayerKnapsackPanel/UIKnapsackSlot.cs
@@ -4,12 +4,12 @@ namespace MMORPG.UI
 {
     public class UIKnapsackSlot : UISlotBase
     {
-        public Image Image { get; protected set; }
+        public Image Image;
 
         protected override void Awake()
         {
             base.Awake();
-            Image = transform.Find("Icon").GetComponent<Image>();
+            //Image = transform.Find("Icon").GetComponent<Image>();
         }
        
     }
