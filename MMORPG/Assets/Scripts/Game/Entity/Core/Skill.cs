ca28e40c3e2a0f7419b098405fb8ec2027b5d748
计都 <119787492+fuyouawa@users.noreply.github.com>
2024-06-10 19:16:23 +0800

    完善技能框架

--- MMORPG/Assets/Scripts/Game/Entity/Core/Skill.cs
+++ MMORPG/Assets/Scripts/Game/Entity/Core/Skill.cs
@@ -1,7 +1,24 @@
-﻿namespace MMORPG.Game
+namespace MMORPG.Game
 {
     public class Skill
     {
-        
+        public CharacterSkillManager Owner;
+        public SkillDefine Define;
+
+        public Skill(CharacterSkillManager owner, SkillDefine define)
+        {
+            Owner = owner;
+            Define = define;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Use()
+        {
+
+        }
     }
 }
