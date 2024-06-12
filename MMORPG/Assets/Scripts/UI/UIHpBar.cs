93644646e27aa9dd5fe8383e7a8582fef90bb57a
yuyuaqwq <1454832774@qq.com>
2024-06-12 22:05:06 +0800

    血条UI

--- MMORPG/Assets/Scripts/UI/UIHpBar.cs
+++ MMORPG/Assets/Scripts/UI/UIHpBar.cs
@@ -5,22 +5,12 @@ using UnityEngine.UI;
 
 namespace MMORPG.UI
 {
-	public class UIHpBar : MonoBehaviour
+    public class UIHpPanel : MonoBehaviour
     {
         public Image HpBar;
         public float Hp, MaxHp = 100;
 
         private float _lerpSpeed = 3;
-        private RectTransform _hpBarRectTransform;
-        private float _initialHpBarWidth;
-
-        private void Start()
-        {
-            // 获取 HpBar 的 RectTransform 组件
-            _hpBarRectTransform = HpBar.GetComponent<RectTransform>();
-            // 记录初始宽度
-            _initialHpBarWidth = _hpBarRectTransform.sizeDelta.x;
-        }
 
         private void Update()
         {
@@ -29,12 +19,8 @@ namespace MMORPG.UI
 
         private void BarFiller()
         {
-            // 计算目标宽度
-            float targetWidth = _initialHpBarWidth * (Hp / MaxHp);
-            // 平滑过渡
-            float newWidth = Mathf.Lerp(_hpBarRectTransform.sizeDelta.x, targetWidth, _lerpSpeed * Time.deltaTime);
-            // 设置新的宽度
-            _hpBarRectTransform.sizeDelta = new Vector2(newWidth, _hpBarRectTransform.sizeDelta.y);
+            HpBar.fillAmount = Mathf.Lerp(HpBar.fillAmount, Hp / MaxHp, _lerpSpeed * Time.deltaTime);
         }
     }
+
 }
