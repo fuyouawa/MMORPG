5905cd373eaa391703cac69521f33d2b7cb994ae
yuyuaqwq <1454832774@qq.com>
2024-06-13 11:50:56 +0800

    excel

---
+++ SERVER/Common/Data/cs/DialogueDefine.cs
@@ -0,0 +1,21 @@
+//
+// Auto Generated Code By excel2json
+// https://neil3d.gitee.io/coding/excel2json.html
+// 1. 每个 Sheet 形成一个 Struct 定义, Sheet 的名称作为 Struct 的名称
+// 2. 表格约定：第一行是变量名称，第二行是变量类型
+
+// Generate From DialogueDefine.xlsx
+
+public class TaskDefine
+{
+	public int ID; // ID
+	public string Decs; // 介绍
+	public string Content; // 内容
+	public int[] Options; // 选项id列表
+	public int jump; // 跳转
+	public int ReceiveTask; // 接取任务id
+	public int SubmitTask; // 提交任务id
+}
+
+
+// End of Auto Generated Code
---
+++ SERVER/Common/Data/cs/TaskDefine.cs
@@ -0,0 +1,18 @@
+//
+// Auto Generated Code By excel2json
+// https://neil3d.gitee.io/coding/excel2json.html
+// 1. 每个 Sheet 形成一个 Struct 定义, Sheet 的名称作为 Struct 的名称
+// 2. 表格约定：第一行是变量名称，第二行是变量类型
+
+// Generate From TaskDefine.xlsx
+
+public class TaskDefine
+{
+	public int ID; // 任务ID
+	public string Name; // 名称
+	public string Type; // 任务类型
+	public string Decs; // 介绍
+}
+
+
+// End of Auto Generated Code
