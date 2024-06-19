6b685f2e2f32a0c8cae0181d354e4e594e222b77
yuyuaqwq <1454832774@qq.com>
2024-06-19 18:10:09 +0800

    奖励系统初步完成

---
+++ SERVER/GameServer/KillSystem/KillManager.cs
@@ -0,0 +1,16 @@
+﻿using GameServer.PlayerSystem;
+using MMORPG.Common.Proto.Npc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.KillSystem
+{
+    public class KillManager
+    {
+        public Player PlayerOwner;
+
+    }
+}
