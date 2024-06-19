819410d3567bac4d3fc702f9f4a57583a431c8c1
yuyuaqwq <1454832774@qq.com>
2024-06-19 19:53:21 +0800

    修复命中率异常的问题

--- SERVER/Common/Proto/Entity/NetTransform.cs
+++ SERVER/Common/Proto/Entity/NetTransform.cs
@@ -39,6 +39,7 @@ namespace MMORPG.Common.Proto.Entity {
 
   }
   #region Messages
+  [global::System.Diagnostics.DebuggerDisplayAttribute("{ToString(),nq}")]
   public sealed partial class NetTransform : pb::IMessage<NetTransform>
   #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
       , pb::IBufferMessage
