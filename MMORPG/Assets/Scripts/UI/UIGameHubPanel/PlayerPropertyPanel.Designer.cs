08f8feac7b4a9140f451558aa8725508a6c0b74b
计都 <119787492+fuyouawa@users.noreply.github.com>
2024-06-21 14:44:11 +0800

    绑定

--- MMORPG/Assets/Scripts/UI/UIGameHubPanel/PlayerPropertyPanel.Designer.cs
+++ MMORPG/Assets/Scripts/UI/UIGameHubPanel/PlayerPropertyPanel.Designer.cs
@@ -1,10 +1,16 @@
-// Generate Id:2c7143d3-a2fb-41ab-a6b5-69ca0facc7d8
+// Generate Id:689814bb-bdd5-49a1-8fbe-2ee7a604a770
 using UnityEngine;
 
 namespace MMORPG.Game
 {
 	public partial class PlayerPropertyPanel : QFramework.IController
 	{
+
+		public UnityEngine.UI.Text TextMpPercentage;
+
+		public UnityEngine.UI.Text TextHpPercentage;
+
+		public UnityEngine.UI.Text TextLevel;
 
 		QFramework.IArchitecture QFramework.IBelongToArchitecture.GetArchitecture()=>MMORPG.Game.GameApp.Interface;
 	}
