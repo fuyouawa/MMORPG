2a331696f623ad2845dca6cb738369ab0af4e449
计都 <119787492+fuyouawa@users.noreply.github.com>
2024-06-28 11:02:32 +0800

    小重构

---
+++ MMORPG/Assets/Scripts/UI/UIGameHubPanel/DialoguePanel.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using QFramework;
+using MMORPG.Command;
+using MMORPG.Event;
+using MMORPG.Game;
+using MMORPG.System;
+using Newtonsoft.Json;
+
+namespace MMORPG.UI
+{
+	public partial class DialoguePanel : ViewController
+	{
+        public UIDialogueOptionButton OptionButtonPrefab;
+
+        void Start()
+		{
+			// Code Here
+		}
+
+        public void Interact()
+        {
+            // 没有选项按钮才允许点击面板继续对话
+            if (GroupOptionBox.childCount == 0)
+            {
+                this.SendCommand(new InteractCommand(0));
+            }
+        }
+
+        public void ClearOptionButton()
+        {
+            for (int i = GroupOptionBox.childCount; i > 0; i--)
+            {
+                Destroy(GroupOptionBox.GetChild(i).gameObject);
+            }
+        }
+
+        public void OnInteract(InteractEvent e)
+        {
+            var dataManagerSystem = this.GetSystem<IDataManagerSystem>();
+            var dialogueDefine = dataManagerSystem.GetDialogueDefine(e.Resp.DialogueId);
+            var entityManagerSystem = this.GetSystem<IEntityManagerSystem>();
+            var entity = entityManagerSystem.EntityDict[e.Resp.EntityId];
+            TextName.text = entity.UnitDefine.Name;
+            Content.text = dialogueDefine.Content;
+            ClearOptionButton();
+
+            if (!string.IsNullOrEmpty(dialogueDefine.Options))
+            {
+                var options = JsonConvert.DeserializeObject<int[]>(dialogueDefine.Options);
+                int j = 0;
+                foreach (var option in options)
+                {
+                    var tmpDefine = dataManagerSystem.GetDialogueDefine(option);
+                    var button = Instantiate(OptionButtonPrefab, GroupOptionBox);
+                    var script = button.GetComponent<UIDialogueOptionButton>();
+                    script.Content.text = tmpDefine.Content;
+                    script.Idx = ++j;
+                }
+            }
+        }
+    }
+}
