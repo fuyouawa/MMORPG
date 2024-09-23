e58c8b11aab1eebaf79247b494829bf3e3fd7e64
arlozen <arlozen.dev@gmail.com>
2024-09-23 15:20:42 +0800

    完善配置注释，readme文档

--- SERVER/GameServer/Db/DbConfig.cs
+++ SERVER/GameServer/Db/DbConfig.cs
@@ -8,10 +8,25 @@ namespace GameServer.Db
 {
     public class DbConfig
     {
+        /// <summary>
+        /// 数据库Host
+        /// </summary>
         public static readonly string Host = "127.0.0.1";
+        /// <summary>
+        /// 数据库端口
+        /// </summary>
         public static readonly int Port = 3306;
+        /// <summary>
+        /// 数据库用户名
+        /// </summary>
         public static readonly string User = "root";
+        /// <summary>
+        /// 数据库密码
+        /// </summary>
         public static readonly string Password = "root";
+        /// <summary>
+        /// 数据库名称
+        /// </summary>
         public static readonly string DbName = "MMORPG";
     }
 }
